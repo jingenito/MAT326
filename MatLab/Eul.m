37b59c3264bf8a2ecbf8e32e927e1c313c7bbd4c
Joe <jingenito1530@gmail.com>
2019-03-27 16:48:20 -0400

    started working on Matlab2 assignment

---
+++ MatLab/Eul.m
@@ -0,0 +1,10 @@
+function [x,y] = Eul(h,x0,y0,xf)
+k = 1;
+x(k) = x0;
+y(k) = y0;
+    while(x(k) + h <= xf + 10^-10)
+        x(k+1) = x(k) + h;
+        y(k+1) = y(k) + h*(x(k) + y(k));
+        k = k + 1;
+    end
+end
