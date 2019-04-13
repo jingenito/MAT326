487adf066064acc6a71df852f469d3d327d2cd49
jingenito <jingenito1530@gmail.com>
2019-04-13 17:31:18 -0400

    fixed exercise 6 functions

--- MatLab/Eul_6.m
+++ MatLab/Eul_6.m
@@ -4,7 +4,7 @@ x(k) = x0;
 y(k) = y0; 
     while(x(k) + h <= xf + 10^-10) 
         x(k+1) = x(k) + h; 
-        y(k+1) = y(k) + h*((-9*x(k)^2)/2*y(k)); 
+        y(k+1) = y(k) + h*((-9*x(k)^2)/(2*y(k))); 
         k = k + 1; 
     end 
 end
