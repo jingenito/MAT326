aee9fa3a1094ad7dbdebc5a83702498be49d2e5d
Joe <jingenito1530@gmail.com>
2019-04-13 14:13:25 -0400

    work done on functions for project2

--- MatLab/Eul_5.m
+++ MatLab/Eul_5.m
@@ -1,10 +1,10 @@
-function [x,y] = Eul(h,x0,y0,xf)
+function [x,y] = Eul_5(h,x0,y0,xf)
 k = 1;
 x(k) = x0;
 y(k) = y0;
     while(x(k) + h <= xf + 10^-10)
         x(k+1) = x(k) + h;
-        y(k+1) = y(k) + h*(x(k) * exp(3*x(k)) - 2 * y(k));
+        y(k+1) = y(k) + h*(-2*y(k));
         k = k + 1;
     end
 end
