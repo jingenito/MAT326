a6f8b8bddd698e30920e7667ce58dc9df1007339
jingenito <jingenito1530@gmail.com>
2019-04-13 21:15:14 -0400

    Added second part functions to question 6

---
+++ MatLab/Eul_6_2.m
@@ -0,0 +1,10 @@
+function [x,y] = Eul_6_2(h,x0,y0,xf)
+k = 1; 
+x(k) = x0; 
+y(k) = y0; 
+    while(x(k) + h <= xf + 10^-10) 
+        x(k+1) = x(k) + h; 
+        y(k+1) = y(k) + h*((cos(x(k)-y(k)))/(1+x(k))); 
+        k = k + 1; 
+    end 
+end
---
+++ MatLab/Eul_improved_6_2.m
@@ -0,0 +1,10 @@
+function [x,y] = Eul_improved_6_2(h, x0, y0, xf) 
+k = 1;
+x(k) = x0;  
+y(k) = y0;  
+while (x(k)+h<=xf+10^-10) 
+    x(k+1) = x(k)+h; 
+    y_predict = y(k)+h*((cos(x(k)-y(k)))/(1+x(k))); 
+    y(k+1) = y(k)+0.5*h*((cos(x(k)-y(k)))/(1+x(k)) + ((cos(x(k+1)-y_predict))/(1+x(k+1)))); 
+    k = k+1; 
+end
