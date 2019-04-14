b60ed48b4eed4ea74d93bab8027205f926f07de4
jingenito <jingenito1530@gmail.com>
2019-04-13 22:14:27 -0400

    modified second part of question 6

--- MatLab/Eul_6_2.m
+++ MatLab/Eul_6_2.m
@@ -4,7 +4,7 @@ x(k) = x0;
 y(k) = y0; 
     while(x(k) + h <= xf + 10^-10) 
         x(k+1) = x(k) + h; 
-        y(k+1) = y(k) + h*((cos(x(k)-y(k)))/(1+x(k))); 
+        y(k+1) = y(k) + h*((cos(x(k))-y(k))/(1+x(k))); 
         k = k + 1; 
     end 
 end
--- MatLab/Eul_improved_6_2.m
+++ MatLab/Eul_improved_6_2.m
@@ -4,7 +4,7 @@ x(k) = x0;
 y(k) = y0;  
 while (x(k)+h<=xf+10^-10) 
     x(k+1) = x(k)+h; 
-    y_predict = y(k)+h*((cos(x(k)-y(k)))/(1+x(k))); 
-    y(k+1) = y(k)+0.5*h*((cos(x(k)-y(k)))/(1+x(k)) + ((cos(x(k+1)-y_predict))/(1+x(k+1)))); 
+    y_predict = y(k)+h*((cos(x(k))-y(k))/(1+x(k))); 
+    y(k+1) = y(k)+0.5*h*((cos(x(k))-y(k))/(1+x(k)) + ((cos(x(k+1))-y_predict)/(1+x(k+1)))); 
     k = k+1; 
 end
