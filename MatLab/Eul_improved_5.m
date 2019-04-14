29f6cece6bf4e2a75e75110046cf695220b86dfa
jingenito <jingenito1530@gmail.com>
2019-04-13 21:14:42 -0400

    fixed previous questions that were not done correctly

--- MatLab/Eul_improved_5.m
+++ MatLab/Eul_improved_5.m
@@ -1,10 +1,10 @@
 function [x,y] = Eul_improved_5(h, x0, y0, xf)
-
-k = 1;x(k) = x0; 
+k = 1;
+x(k) = x0; 
 y(k) = y0; 
 while (x(k)+h<=xf+10^-10)
     x(k+1) = x(k)+h;
     y_predict = y(k)+h*(-2*y(k));
-    y(k+1) = y(k)+0.5*h*(-2*y(k)) + (x(k+1)+y_predict);
+    y(k+1) = y(k)+0.5*h*((-2*y(k)) + (-2*y_predict));
     k = k+1;
 end
